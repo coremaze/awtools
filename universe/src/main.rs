bdcd74c8754a6b6a665150d6b5c9c389748f9b07
ChrisMiuchiz <chris000005@gmail.com>
2022-07-02 23:16:04 -0400

    use new modules, use localhost port

--- universe/src/main.rs
+++ universe/src/main.rs
@@ -1,14 +1,15 @@
 use aw_core::*;
-use std::{
-    net::{Ipv4Addr, TcpListener},
-    str::FromStr,
-};
+use std::{net::Ipv4Addr, str::FromStr};
 
 mod client;
-pub use client::Client;
+pub use client::{Client, ClientType};
 mod universe_server;
 pub use universe_server::UniverseServer;
+mod attributes;
+pub mod license;
+pub use attributes::{send_attributes, Attribute};
+pub mod packet_handler;
 
 fn main() {
-    UniverseServer::new(Ipv4Addr::from_str("0.0.0.0").unwrap(), 6670).run();
+    UniverseServer::new(Ipv4Addr::from_str("127.0.0.1").unwrap(), 6670).run();
 }
